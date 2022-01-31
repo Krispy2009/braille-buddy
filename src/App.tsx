5678791e352c29843429342d11ce19848c1fac0f
Kristi Achilleos <krispy2009@gmail.com>
2022-01-31 19:39:53 +0200

    add braille cell

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React, { ReactElement } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-type DotProps = {cellId: number, isOn: boolean}
-const Dot = ({cellId, isOn}: DotProps): ReactElement => {
-  return <span className={isOn ? 'on' : 'off'}>{cellId}</span>
-}
 
-const  Board = () =>  <div>
-  <h2> This is your board</h2>
-  <div>
+const Cell : React.FC = () => {
+  return <div className="cell">
   <Dot cellId={1} isOn= {false}/>
   <Dot cellId={2} isOn= {false}/>
   <Dot cellId={3} isOn= {false}/>
@@ -17,6 +12,29 @@ const  Board = () =>  <div>
   <Dot cellId={5} isOn= {false}/>
   <Dot cellId={6} isOn= {false}/>
   </div>
+}
+
+
+type DotProps = {cellId: number, isOn: boolean,  className?: string, }
+
+const Dot = styled(({cellId, isOn, className, }: DotProps) => {
+ const [checked, setchecked] = React.useState(isOn);
+
+  const onClick = () => {
+    setchecked(!checked)
+  }
+
+  return <span onClick={onClick} className={className} style={{backgroundColor: `${checked? 'black' : 'white'}`}}>{cellId}</span>
+})`
+    border: 1px solid black;
+    border-radius: 40px;
+    padding: 4px 10px;`
+
+
+
+const  Board = () =>  <div>
+  <h2> A braille cell</h2>
+  <Cell/>
 
 </div>
 
