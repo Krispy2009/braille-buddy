dad4585a880c544882f3185ec763788a87721125
Kristi Achilleos <Krispy2009@gmail.com>
2022-02-01 19:39:37 +0200

    lift state up to Board and refactor

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,9 +1,66 @@
 import React from "react";
 import { Cell } from "../Cell/Cell";
+import { CellType } from "../Dot/Dot";
+import mappings from "./mappings.json";
 
-export const Board = () => (
-  <div>
-    <h2> braille cell</h2>
-    <Cell />
-  </div>
-);
+export const Board = () => {
+  const [positions, setCells] = React.useState({
+    dot1: false,
+    dot2: false,
+    dot3: false,
+    dot4: false,
+    dot5: false,
+    dot6: false,
+  });
+
+  const [letterEN, setLetterEN] = React.useState("");
+  const [letterGR, setLetterGR] = React.useState("");
+
+  const onClick = (dot: keyof CellType) => {
+    const newCells: CellType = { ...positions };
+    newCells[dot] = !positions[dot];
+    setCells(newCells);
+
+    const translatedLetterEN: string = translateCellToLetter(newCells);
+    setLetterEN(translatedLetterEN);
+
+    const translatedLetterGR: string = translateCellToLetter(newCells, "gr");
+    setLetterGR(translatedLetterGR);
+  };
+
+  const translateCellToBitmap = (cells: CellType): string => {
+    let bitmap: string = "";
+    for (const dot in positions) {
+      const dot2: keyof CellType = dot as keyof CellType;
+      bitmap += cells[dot2] ? "1" : "0";
+    }
+    return bitmap;
+  };
+
+  const translateCellToLetter = (
+    cells: CellType,
+    language: string = "en"
+  ): string => {
+    const bitmap: string = translateCellToBitmap(cells);
+    if (language === "en") {
+      return (mappings.english as any)[bitmap];
+    } else {
+      return (mappings.greek as any)[bitmap];
+    }
+  };
+
+  return (
+    <div>
+      <h2> braille cell</h2>
+      <Cell positions={positions} onClick={onClick} />
+      <div className={"results"}>
+        <div className={"result"}>
+          <span className={"TranslatedLetter"}>{letterEN}</span> English
+        </div>
+        <div className={"result"}>
+          <span className={"TranslatedLetter"}>{letterGR} </span> Greek
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,103 +1,52 @@
 import React from "react";
 import { Dot, CellType } from "../Dot/Dot";
-import positions from "./positions.json";
 
-export const Cell: React.FC = () => {
-  const [cells, setCells] = React.useState({
-    dot1: false,
-    dot2: false,
-    dot3: false,
-    dot4: false,
-    dot5: false,
-    dot6: false,
-  });
+type CellProps = {
+  positions: CellType;
+  onClick: (dot: keyof CellType) => void;
+};
 
-  const [letterEN, setLetterEN] = React.useState("");
-  const [letterGR, setLetterGR] = React.useState("");
-
-  const onClick = (dot: keyof CellType) => {
-    const newCells: CellType = { ...cells };
-    newCells[dot] = !cells[dot];
-    setCells(newCells);
-
-    const translatedLetterEN: string = translateCellToLetter(newCells);
-    setLetterEN(translatedLetterEN);
-
-    const translatedLetterGR: string = translateCellToLetter(newCells, "gr");
-    setLetterGR(translatedLetterGR);
-  };
-
-  const translateCellToBitmap = (cells: CellType): string => {
-    let bitmap: string = "";
-    for (const dot in cells) {
-      const dot2: keyof CellType = dot as keyof CellType;
-      bitmap += cells[dot2] ? "1" : "0";
-    }
-    return bitmap;
-  };
-
-  const translateCellToLetter = (
-    cells: CellType,
-    language: string = "en"
-  ): string => {
-    const bitmap: string = translateCellToBitmap(cells);
-    if (language === "en") {
-      return (positions.english as any)[bitmap];
-    } else {
-      return (positions.greek as any)[bitmap];
-    }
-  };
-
+export const Cell = ({ positions, onClick }: CellProps) => {
   return (
-    <div>
-      <div className="cell">
-        <Dot
-          className={"dot1"}
-          cellId={"dot1"}
-          isOn={cells.dot1}
-          onClick={onClick}
-        />
-        <Dot
-          className={"dot2"}
-          cellId={"dot2"}
-          isOn={cells.dot2}
-          onClick={onClick}
-        />
+    <div className="cell">
+      <Dot
+        className={"dot1"}
+        cellId={"dot1"}
+        isOn={positions.dot1}
+        onClick={onClick}
+      />
+      <Dot
+        className={"dot2"}
+        cellId={"dot2"}
+        isOn={positions.dot2}
+        onClick={onClick}
+      />
 
-        <Dot
-          className={"dot3"}
-          cellId={"dot3"}
-          isOn={cells.dot3}
-          onClick={onClick}
-        />
-        <Dot
-          className={"dot4"}
-          cellId={"dot4"}
-          isOn={cells.dot4}
-          onClick={onClick}
-        />
+      <Dot
+        className={"dot3"}
+        cellId={"dot3"}
+        isOn={positions.dot3}
+        onClick={onClick}
+      />
+      <Dot
+        className={"dot4"}
+        cellId={"dot4"}
+        isOn={positions.dot4}
+        onClick={onClick}
+      />
 
-        <Dot
-          className={"dot5"}
-          cellId={"dot5"}
-          isOn={cells.dot5}
-          onClick={onClick}
-        />
-        <Dot
-          className={"dot6"}
-          cellId={"dot6"}
-          isOn={cells.dot6}
-          onClick={onClick}
-        />
-      </div>
-      <div className={"results"}>
-        <div className={"result"}>
-          <span className={"TranslatedLetter"}>{letterEN}</span> English
-        </div>
-        <div className={"result"}>
-          <span className={"TranslatedLetter"}>{letterGR} </span> Greek
-        </div>
-      </div>
+      <Dot
+        className={"dot5"}
+        cellId={"dot5"}
+        isOn={positions.dot5}
+        onClick={onClick}
+      />
+      <Dot
+        className={"dot6"}
+        cellId={"dot6"}
+        isOn={positions.dot6}
+        onClick={onClick}
+      />
     </div>
   );
 };
