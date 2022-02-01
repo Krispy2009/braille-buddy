d82a7745c2a7ce7c5412f0d36b614326f1c6502e
Kristi Achilleos <Krispy2009@gmail.com>
2022-02-01 13:06:34 +0200

    add prettier + format

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,72 @@
-import React from 'react';
-import styled from 'styled-components';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
 
+type CellType = {
+  dot1: boolean;
+  dot2: boolean;
+  dot3: boolean;
+  dot4: boolean;
+  dot5: boolean;
+  dot6: boolean;
+};
 
-type CellType = {dot1: boolean, dot2: boolean, dot3: boolean, dot4: boolean, dot5:boolean, dot6:boolean}
+const Cell: React.FC = () => {
+  const [cells, setCells] = React.useState({
+    dot1: false,
+    dot2: false,
+    dot3: false,
+    dot4: false,
+    dot5: false,
+    dot6: false,
+  });
 
-const Cell : React.FC = () => {
+  const onClick = (dot: keyof CellType) => {
+    const newCells: CellType = { ...cells };
+    newCells[dot] = !cells[dot];
+    setCells(newCells);
+  };
 
-  const [cells, setCells] = React.useState({dot1: false, dot2: false, dot3: false, dot4: false, dot5: false, dot6:false})
+  return (
+    <div className="cell">
+      <Dot cellId={"dot1"} isOn={cells.dot1} onClick={onClick} />
+      <Dot cellId={"dot2"} isOn={cells.dot2} onClick={onClick} />
+      <Dot cellId={"dot3"} isOn={cells.dot3} onClick={onClick} />
+      <Dot cellId={"dot4"} isOn={cells.dot4} onClick={onClick} />
+      <Dot cellId={"dot5"} isOn={cells.dot5} onClick={onClick} />
+      <Dot cellId={"dot6"} isOn={cells.dot6} onClick={onClick} />
+    </div>
+  );
+};
 
-  const onClick = (dot : number) => {
-    const newCells : CellType = {...cells}
-    const dotIdx: string =  `dot${dot}`
-    newCells[dotIdx] = !cells[dotIdx]
-    setCells(newCells)
-  }
+type DotProps = {
+  cellId: keyof CellType;
+  isOn: boolean;
+  className?: string;
+  onClick: (dot: keyof CellType) => void;
+};
 
-  return <div className="cell">
-  <Dot cellId={1} isOn= {cells.dot1}/>
-  <Dot cellId={2} isOn= {cells.dot2}/>
-  <Dot cellId={3} isOn= {cells.dot3}/>
-  <Dot cellId={4} isOn= {cells.dot4}/>
-  <Dot cellId={5} isOn= {cells.dot5}/>
-  <Dot cellId={6} isOn= {cells.dot6}/>
-  </div>
-}
-
-
-type DotProps = {cellId: number, isOn: boolean,  className?: string, }
-
-const Dot = styled(({cellId, isOn, className, }: DotProps) => {
- const [checked, setchecked] = React.useState(isOn);
-
-
-
-  return <span onClick={onClick} className={className} style={{backgroundColor: `${checked? 'black' : 'white'}`}}>{cellId}</span>
+const Dot = styled(({ cellId, isOn, className }: DotProps) => {
+  return (
+    <span
+      className={className}
+      style={{ backgroundColor: `${isOn ? "black" : "white"}` }}
+    >
+      {cellId}
+    </span>
+  );
 })`
-    border: 1px solid black;
-    border-radius: 40px;
-    padding: 4px 10px;`
+  border: 1px solid black;
+  border-radius: 40px;
+  padding: 4px 10px;
+`;
 
-
-
-const  Board = () =>  <div>
-  <h2> A braille cell</h2>
-  <Cell/>
-
-</div>
-
-
+const Board = () => (
+  <div>
+    <h2> A braille cell</h2>
+    <Cell />
+  </div>
+);
 
 function App() {
   return (
